85c883baa68e96b165b8fb7ec724e64aa1c4e29e
MeledakCik <kakangdanadam@gmail.com>
2026-07-15 00:38:23 +0700

    asdsad

--- app/layout.tsx
+++ app/layout.tsx
@@ -156,6 +156,8 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full bg-black antialiased`}
     >
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Script
           nonce={nonce}
           id="person-jsonld"
