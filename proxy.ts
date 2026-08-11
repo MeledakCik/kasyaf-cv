1ce062da0c8ed8258a38c0a966a5051854446aad
MeledakCik <kakangdanadam@gmail.com>
2026-08-11 19:11:53 +0700

    asdadasdadasdasa

--- proxy.ts
+++ proxy.ts
@@ -325,6 +325,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/webpack-hmr).*)',
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)',
   ],
 };
